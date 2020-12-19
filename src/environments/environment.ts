66975a9c38dead55d6210b4bd6b71286586ba5d8
Adeolaemmanuel <adeolaemmanuel006@gmail.com>
2020-12-19 20:28:14 +0100

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,7 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: "AIzaSyCras_dZVbeU60fHHvkD_DgAD3BPthknNQ",
-    authDomain: "hopeful-machine-272310.firebaseapp.com",
-    databaseURL: "https://hopeful-machine-272310.firebaseio.com",
-    projectId: "hopeful-machine-272310",
-    storageBucket: "hopeful-machine-272310.appspot.com",
-    messagingSenderId: "303911473886",
-    appId: "1:303911473886:web:2f64bc661b388102af284f",
-    measurementId: "G-2TNZ7XDV2Q"
+
   }
 };
 
